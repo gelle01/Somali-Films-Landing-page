ee74f225a4de53b4d57e4d496a0a05c0b674307f
Abdisalam Gelle <umadda2009@gmail.com>
2020-12-19 22:33:29 +0100

    Adding new files

--- js/main.js
+++ js/main.js
@@ -19,3 +19,15 @@ function removeShow() {
 }
 
 tabItems.forEach((item) => item.addEventListener("click", selectItem));
+
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
+
+signUpButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
+});
